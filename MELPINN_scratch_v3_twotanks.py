fcfa8a8667d234de3598e41dca7b74d7629995b3
Woong <sunwoongy@gmail.com>
2024-08-20 09:21:31 +0900

    240611

--- MELPINN_scratch_v3_twotanks.py
+++ MELPINN_scratch_v3_twotanks.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import argparse
 
+
 def install(package):
 	subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
@@ -30,6 +31,8 @@ import time
 from tqdm import tqdm
 import os
 
+torch.set_num_threads(12)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 if device.type == 'cuda':
@@ -38,15 +41,16 @@ if device.type == 'cuda':
 
 # Argument parsing
 parser = argparse.ArgumentParser(description='Train a Physics-Informed Neural Network (PINN).')
-parser.add_argument('-e', '--epochs', type=int, default=5000, help='Number of epochs for training.')
+parser.add_argument('-e', '--epochs', type=int, default=30000, help='Number of epochs for training.')
 parser.add_argument('-N', '--N_collo', type=int, default=5000, help='Number of collocation points.')
-parser.add_argument('-hl', '--hidden_layers', type=int, nargs='+', default=[64] * 7, help='List of hidden layers sizes.')
+parser.add_argument('-hl', '--hidden_layers', type=int, nargs='+', default=[64] * 7,
+                    help='List of hidden layers sizes.')
 parser.add_argument('-t', '--end_time', type=int, default=2758, help='End time for the simulation.')
-parser.add_argument('-BC', '--use_hard_BC', type=str, default='False', help='Use hard boundary conditions (True/False).')
+parser.add_argument('-BC', '--use_hard_BC', type=str, default='True', help='Use hard boundary conditions (True/False).')
 
 args = parser.parse_args()
 
-N_tank = 6
+N_tank = 2
 epochs = args.epochs
 N_collo = args.N_collo
 hidden_layers = args.hidden_layers
@@ -55,6 +59,7 @@ use_hard_BC = args.use_hard_BC.lower() in ['t', 'true']
 
 # Construct the folder name based on the argument values
 folder_name = f"figures/e{epochs}-N{N_collo}-hl{'x'.join(map(str, hidden_layers))}-t{end_time}-BC{use_hard_BC}"
+
 
 # Function to clear the specified directory or create it if it does not exist
 def make_or_clear_figures_directory(folder_name):
@@ -65,8 +70,10 @@ def make_or_clear_figures_directory(folder_name):
 		if os.path.isfile(file_path):
 			os.unlink(file_path)
 
+
 # Clear or create the specified figures directory
 make_or_clear_figures_directory(folder_name)
+
 
 # Neural Network Definition
 class PINN(nn.Module):
@@ -122,24 +129,11 @@ def residual(net, t):
 	a_void = [void_fraction(height[:, [i]]) for i in range(N_tank - 1)]
 
 	momentum1 = L * du_dt[0] - g * height[:, [0]] + K * torch.abs(vel[:, [0]]) * vel[:, [0]] / 2
-	momentum2 = L * du_dt[1] - g * height[:, [1]] + K * torch.abs(vel[:, [1]]) * vel[:, [1]] / 2
-	momentum3 = L * du_dt[2] - g * height[:, [2]] + K * torch.abs(vel[:, [2]]) * vel[:, [2]] / 2
-	momentum4 = L * du_dt[3] - g * height[:, [3]] + K * torch.abs(vel[:, [3]]) * vel[:, [3]] / 2
-	momentum5 = L * du_dt[4] - g * height[:, [4]] + K * torch.abs(vel[:, [4]]) * vel[:, [4]] / 2
 
-	continuity1 = du_dt[5] * rho * A_t + rho * vel[:, [0]] * A_p * (1 - a_void[0])
-	continuity2 = du_dt[6] * rho * A_t + rho * vel[:, [1]] * A_p * (1 - a_void[1]) - rho * vel[:, [0]] * A_p * (
-			1 - a_void[0])
-	continuity3 = du_dt[7] * rho * A_t + rho * vel[:, [2]] * A_p * (1 - a_void[2]) - rho * vel[:, [1]] * A_p * (
-			1 - a_void[1])
-	continuity4 = du_dt[8] * rho * A_t + rho * vel[:, [3]] * A_p * (1 - a_void[3]) - rho * vel[:, [2]] * A_p * (
-			1 - a_void[2])
-	continuity5 = du_dt[9] * rho * A_t + rho * vel[:, [4]] * A_p * (1 - a_void[4]) - rho * vel[:, [3]] * A_p * (
-			1 - a_void[3])
-	continuity6 = du_dt[10] * rho * A_t - rho * vel[:, [4]] * A_p * (1 - a_void[4])
+	continuity1 = du_dt[1] * rho * A_t + rho * vel[:, [0]] * A_p * (1 - a_void[0])
+	continuity2 = du_dt[2] * rho * A_t - rho * vel[:, [0]] * A_p * (1 - a_void[0])
 
-	return momentum1, momentum2, momentum3, momentum4, momentum5, \
-		continuity1, continuity2, continuity3, continuity4, continuity5, continuity6
+	return momentum1, continuity1, continuity2
 
 
 # Impose hard BC/IC
@@ -186,22 +180,13 @@ def train_pinn(weights=None, use_hard_BC=True, epochs=30000, N_collo=2000, end_t
 		optimizer.zero_grad()
 
 		# Physics loss
-		momentum1, momentum2, momentum3, momentum4, momentum5, \
-			continuity1, continuity2, continuity3, continuity4, continuity5, continuity6 = residual(net, t_collocation)
+		momentum1, continuity1, continuity2 = residual(net, t_collocation)
 
 		# Calculate individual losses
 		loss_momentum1 = nn.MSELoss()(momentum1, torch.zeros_like(momentum1))
-		loss_momentum2 = nn.MSELoss()(momentum2, torch.zeros_like(momentum2))
-		loss_momentum3 = nn.MSELoss()(momentum3, torch.zeros_like(momentum3))
-		loss_momentum4 = nn.MSELoss()(momentum4, torch.zeros_like(momentum4))
-		loss_momentum5 = nn.MSELoss()(momentum5, torch.zeros_like(momentum5))
 
 		loss_continuity1 = nn.MSELoss()(continuity1, torch.zeros_like(continuity1))
 		loss_continuity2 = nn.MSELoss()(continuity2, torch.zeros_like(continuity2))
-		loss_continuity3 = nn.MSELoss()(continuity3, torch.zeros_like(continuity3))
-		loss_continuity4 = nn.MSELoss()(continuity4, torch.zeros_like(continuity4))
-		loss_continuity5 = nn.MSELoss()(continuity5, torch.zeros_like(continuity5))
-		loss_continuity6 = nn.MSELoss()(continuity6, torch.zeros_like(continuity6))
 
 		# Initial condition loss
 		if not use_hard_BC:
@@ -213,10 +198,8 @@ def train_pinn(weights=None, use_hard_BC=True, epochs=30000, N_collo=2000, end_t
 			loss_ic = 0
 
 		# Apply weights to individual losses
-		loss = (weights[0] * loss_momentum1 + weights[1] * loss_momentum2 + weights[2] * loss_momentum3 +
-		        weights[3] * loss_momentum4 + weights[4] * loss_momentum5 + weights[5] * loss_continuity1 +
-		        weights[6] * loss_continuity2 + weights[7] * loss_continuity3 + weights[8] * loss_continuity4 +
-		        weights[9] * loss_continuity5 + weights[10] * loss_continuity6) + loss_ic
+		loss = (weights[0] * loss_momentum1 + weights[5] * loss_continuity1 +
+		        weights[6] * loss_continuity2) + loss_ic
 
 		loss.backward()  # No need to retain the graph now
 		optimizer.step()
@@ -224,8 +207,8 @@ def train_pinn(weights=None, use_hard_BC=True, epochs=30000, N_collo=2000, end_t
 
 		losses = {
 			'Total Loss': loss.item(),
-			'Momentum Losses': f'{loss_momentum1.item():.4f}, {loss_momentum2.item():.4f}, {loss_momentum3.item():.4f}, {loss_momentum4.item():.4f}, {loss_momentum5.item():.4f}',
-			'Continuity Losses': f'{loss_continuity1.item():.4f}, {loss_continuity2.item():.4f}, {loss_continuity3.item():.4f}, {loss_continuity4.item():.4f}, {loss_continuity5.item():.4f}, {loss_continuity6.item():.4f}',
+			'Momentum Losses': f'{loss_momentum1.item():.4f}',
+			'Continuity Losses': f'{loss_continuity1.item():.4f}, {loss_continuity2.item():.4f}',
 			'IC Losses': f'{loss_ic.item():.4f}' if not use_hard_BC else 'N/A'
 		}
 
@@ -241,60 +224,94 @@ def train_pinn(weights=None, use_hard_BC=True, epochs=30000, N_collo=2000, end_t
 
 
 def save_results(net, input, epoch=None, losses=None):
-    input = torch.tensor([[input]], dtype=torch.float32)
-    final_results = net(input).squeeze().detach().cpu().numpy()
+	input = torch.tensor([[input]], dtype=torch.float32)
+	final_results = net(input).squeeze().detach().cpu().numpy()
 
-    with open(f'{folder_name}/tank_height.txt', 'a') as file:
-        if epoch is not None:
-            file.write(f"epoch={epoch}\n")
-        for tank in range(N_tank):
-            file.write(f"Tank {tank + 1} height: {final_results[N_tank - 1 + tank]:.6f}\n")
-        file.write("\n")
-        if losses is not None:
-            file.write(f"Total Loss: {losses['Total Loss']:.4f}\n")
-            file.write(f"Momentum Losses: {losses['Momentum Losses']}\n")
-            file.write(f"Continuity Losses: {losses['Continuity Losses']}\n")
-            file.write(f"IC Losses: {losses['IC Losses']}\n")
-            file.write("\n")
+	with open(f'{folder_name}/tank_height.txt', 'a') as file:
+		if epoch is not None:
+			file.write(f"epoch={epoch}\n")
+		for tank in range(N_tank):
+			file.write(f"Tank {tank + 1} height: {final_results[N_tank - 1 + tank]:.6f}\n")
+		file.write("\n")
+		if losses is not None:
+			file.write(f"Total Loss: {losses['Total Loss']:.4f}\n")
+			file.write(f"Momentum Losses: {losses['Momentum Losses']}\n")
+			file.write(f"Continuity Losses: {losses['Continuity Losses']}\n")
+			file.write(f"IC Losses: {losses['IC Losses']}\n")
+			file.write("\n")
 
-    if epoch is not None:
-        plot_heights(net, t_end=end_time, epoch=epoch)
+	if epoch is not None:
+		plot_heights(net, t_end=end_time, epoch=epoch)
+		plot_velocities(net, t_end=end_time, epoch=epoch)
 
-    return final_results
+	return final_results
 
 
 def plot_heights(net, t_end=3000, epoch=None):
-    t_start = 0
-    t_values = np.linspace(t_start, t_end, t_end)
-    heights = np.zeros((t_end, N_tank))
+	t_start = 0
+	t_values = np.linspace(t_start, t_end, t_end)
+	heights = np.zeros((t_end, N_tank))
 
-    for i, t in enumerate(t_values):
-        input = torch.tensor([[t]], dtype=torch.float32)
-        u_pred = net(input).squeeze().detach().numpy()
-        heights[i, :] = u_pred[N_tank - 1:]
+	for i, t in enumerate(t_values):
+		input = torch.tensor([[t]], dtype=torch.float32)
+		u_pred = net(input).squeeze().detach().numpy()
+		heights[i, :] = u_pred[N_tank - 1:]
 
-    plt.figure(figsize=(10, 6))
-    for tank in range(N_tank):
-        plt.plot(t_values, heights[:, tank], label=f'Tank {tank + 1} Height')
+	plt.figure(figsize=(10, 6))
+	for tank in range(N_tank):
+		plt.plot(t_values, heights[:, tank], label=f'Tank {tank + 1} Height')
 
-    plt.xlabel('Time (seconds)', fontsize=20)
-    plt.ylabel('Height', fontsize=20)
-    plt.title('Tank Heights Over Time', fontsize=24)
-    plt.legend(fontsize=16)
+	plt.xlabel('Time (seconds)', fontsize=20)
+	plt.ylabel('Height', fontsize=20)
+	plt.title('Tank Heights Over Time', fontsize=24)
+	plt.legend(fontsize=16)
 
-    if epoch is not None:
-        plt.title(f'Tank Heights Over Time (End of Epoch {epoch})', fontsize=24)
-        plt.savefig(f'{folder_name}/heights_epoch_{epoch}.png', dpi=300)
-    else:
-        plt.savefig(f'{folder_name}/Final_heights.png', dpi=300)
+	if epoch is not None:
+		plt.title(f'Tank Heights Over Time (End of Epoch {epoch})', fontsize=24)
+		plt.savefig(f'{folder_name}/heights_epoch_{epoch}.png', dpi=300)
+	else:
+		plt.savefig(f'{folder_name}/Final_heights.png', dpi=300)
 
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-    plt.show()
-    plt.close()
+	plt.xticks(fontsize=16)
+	plt.yticks(fontsize=16)
+	plt.show()
+	plt.close()
+
+
+def plot_velocities(net, t_end=3000, epoch=None):
+	t_start = 0
+	t_values = np.linspace(t_start, t_end, t_end)
+	velocities = np.zeros((t_end, N_tank - 1))
+
+	for i, t in enumerate(t_values):
+		input = torch.tensor([[t]], dtype=torch.float32)
+		u_pred = net(input).squeeze().detach().numpy()
+		velocities[i, :] = u_pred[:N_tank - 1]
+
+	plt.figure(figsize=(10, 6))
+	for tank in range(N_tank-1):
+		plt.plot(t_values, velocities[:, tank], label=f'Tank {tank + 1} Velocity')
+
+	plt.xlabel('Time (seconds)', fontsize=20)
+	plt.ylabel('Velocity', fontsize=20)
+	plt.title('Tank Velocities Over Time', fontsize=24)
+	plt.legend(fontsize=16)
+
+	if epoch is not None:
+		plt.title(f'Tank Velocities Over Time (End of Epoch {epoch})', fontsize=24)
+		plt.savefig(f'{folder_name}/velocities_epoch_{epoch}.png', dpi=300)
+	else:
+		plt.savefig(f'{folder_name}/Final_velocities.png', dpi=300)
+
+	plt.xticks(fontsize=16)
+	plt.yticks(fontsize=16)
+	plt.show()
+	plt.close()
+
 
 # Train and validate the model
 net = train_pinn(weights=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], use_hard_BC=use_hard_BC, epochs=epochs, N_collo=N_collo,
                  end_time=end_time, plot_period=1000, hidden_layers=hidden_layers)
 final_results = save_results(net, input=end_time)
 plot_heights(net, t_end=end_time)
+plot_velocities(net, t_end=end_time)
